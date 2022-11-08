78f4d18724fc4a175c80ad13df9c9e2098cb115e
Maiorusergiu <maiorusergiu@gmail.com>
2022-11-08 20:57:29 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function Add(a, b) {
+return a + b;
+}
+function Subtract(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b) {
+return a * b;
+}
+function Divide(a, b) {
+return a/b;
+}
+function Operate(operator, a, b) {
+return operator(a, b);
+}
+
+//console.log(Operate(Add, 2, 3))
+
